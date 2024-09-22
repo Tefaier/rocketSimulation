rotation fix (but more problems expected)

Simulation/CommandLogic.py
@@ -6,7 +6,7 @@ from scipy.spatial.transform import Rotation
 import numpy.polynomial.polynomial as poly
 
 from Simulation.Entity import SimulationEntity, Rocket
-from ReferenceValues import rocketName, earthName, sunName, marsName
+from Simulation.ReferenceValues import rocketName, earthName, sunName, marsName
 from Simulation.SimulationMath import angleBetweenVectors, getPerpendicularVector, vecNormalize, rotationToVector, \
     setRotationAngle, vectorLerp
 
@@ -30,32 +30,37 @@ class Command:
     def gravityTurnCommand(self, entities: dict[str, SimulationEntity]) -> bool:
         refObj = entities[self.properties["referenceObject"]]
         rocket: Rocket = entities[rocketName]  # just ignore
+        relativeVelocity = rocket.velocity - refObj.velocity
         # angle to have velocity at from radius vector from referenceObject
         targetAttackAngle = self.properties["attackAngleFunction"](np.linalg.norm(refObj.position - rocket.position))
         # choose direction for force
         normVector: np.array
         attackVector: np.array  # direction in which rocket wants velocity to be in
-        if np.linalg.norm(rocket.velocity) < 0.1:
+        # for now it's made so depending on ignoring effect of torque
+        # norm = 1
+        speedRotateForceDirection: np.array
+        decidedDirection: np.array
+        if np.linalg.norm(relativeVelocity) < 0.1:
             normVector = vecNormalize(getPerpendicularVector(refObj.position - rocket.position))
             attackVector = Rotation.from_rotvec(normVector * targetAttackAngle).apply(rocket.position - refObj.position)
+            decidedDirection = vecNormalize(attackVector)
         else:
             # norm = 1
             normVector = np.cross(refObj.position - rocket.position, rocket.velocity)
             attackVector = vecNormalize(setRotationAngle(
                 rotationToVector(
                     rocket.position - refObj.position,
-                    rocket.velocity
+                    relativeVelocity
                 ),
                 targetAttackAngle
             ).apply(rocket.position - refObj.position))
+            speedRotateForceDirection = setRotationAngle(rotationToVector(relativeVelocity, attackVector), 90,
+                                                         True).apply(vecNormalize(relativeVelocity))
+            decidedDirection = vecNormalize(vectorLerp(
+                attackVector,
+                speedRotateForceDirection,
+                self.properties["enforceDirectionRatio"](angleBetweenVectors(relativeVelocity, attackVector))))
 
-        # for now it's made so depending on ignoring effect of torque
-        # norm = 1
-        speedRotateForceDirection = setRotationAngle(rotationToVector(rocket.velocity, attackVector), 90, True).apply(vecNormalize(rocket.velocity))
-        decidedDirection = vecNormalize(vectorLerp(
-            attackVector,
-            speedRotateForceDirection,
-            self.properties["enforceDirectionRatio"](angleBetweenVectors(rocket.velocity, attackVector))))
         # force that is needed to acquire maximum possible resultant force in the direction of decidedDirection
         x = rocket.force[0]
         y = rocket.force[1]
@@ -68,6 +73,7 @@ class Command:
         roots = poly.polyroots([x*x + y*y + z*z - force**2, -2*(x*xt + y*yt + z*zt), xt*xt + yt*yt + zt*zt])
         forceToApply = max(roots) * decidedDirection - rocket.force
         rocket.changeThrusterConfig(np.linalg.norm(forceToApply), forceToApply)
+        print("Relative velocity ",relativeVelocity, "\nRelative position ", rocket.position - refObj.position, "\nDistance ", np.linalg.norm(refObj.position - rocket.position), "\nForce on rocket", rocket.force, "\nApplied force ", forceToApply)
 
         return self.gravityTurnExitCondition(entities)
 

Simulation/Entity.py
@@ -4,9 +4,6 @@ import numpy as np
 from quaternion import quaternion
 from scipy.spatial.transform import Rotation
 
-from Simulation.SimulationMath import rotationToVectorFromBase, vectorUp, rotationToVector
-
-
 class ForceTypes(Enum):
     gravity = 1,
     buoyancy = 2,
@@ -82,6 +79,8 @@ class Rocket(SimulationEntity):
         self.distanceThrusterToCenterOfMass = distanceTTCOM
 
     def changeThrusterConfig(self, thrusterForce: float, forceDirection: np.array):
+        from Simulation.SimulationMath import vectorUp, rotationToVector, vecNormalize
+
         self.thrusterForce = max(min(thrusterForce, self.thrusterForceMax), self.thrusterForceMin)
         # unfinished - Rotation input may affect restriction to be lower than should be
         rotationVec = rotationToVector(self.rotation.apply(vectorUp), forceDirection).as_rotvec()
@@ -93,7 +92,7 @@ class Rocket(SimulationEntity):
             self.thrusterRotation = Rotation.from_rotvec(rotationVecLimited)
 
     def applyAction(self):
-        self.force += self.thrusterForce * (self.rotation * self.thrusterRotation).apply(np.array([0, 0, 1]))
+        self.force += self.thrusterForce * self.thrusterRotation.apply(self.rotation.apply(np.array([0, 0, 1])))
 
         # due to torque being not fully supported yet, it's made this way
         vectorToCenterOfMass = self.distanceThrusterToCenterOfMass * (self.rotation).apply(np.array([0, 0, 1]))

Simulation/Simulation.py
@@ -2,7 +2,7 @@ from typing import List
 
 import numpy as np
 
-from ReferenceValues import *
+from Simulation.ReferenceValues import *
 from Simulation.CommandLogic import Command, CommandType
 from Simulation.Entity import *
 from scipy.spatial.transform import Rotation
@@ -11,7 +11,7 @@ from scipy.interpolate import interp1d
 from Simulation.SimulationMath import distanceBetweenObjects, noRotation, rotationToVector, vectorUp, vecNormalize, rotationToVectorFromBase, earthAtmosphereDensityFunc
 
 def startSimulation(
-        timeUnit = pd.Timedelta(minutes = 1),
+        timeUnit = pd.Timedelta(seconds = 1),
         commands: List[Command] = [
             Command(
                 CommandType.gravityTurn,
@@ -21,8 +21,8 @@ def startSimulation(
                     "maximumDistance": earthRadius + 1e7,
                     "maximumAngleForceToReferenceObject": np.pi * 0.2,
                     "attackAngleFunction": interp1d(
-                        x=[earthRadius, earthRadius + 2e3, earthRadius + 1e4, earthRadius + 2e4, earthRadius + 1e5, earthRadius + 1.1e7],
-                        y=[0, np.pi / 18, np.pi / 7, np.pi / 5, np.pi / 3, np.pi / 2.2],
+                        x=[0, earthRadius, earthRadius + 2e3, earthRadius + 1e4, earthRadius + 2e4, earthRadius + 1e5, earthRadius + 1.1e7],
+                        y=[0, 0, np.pi / 18, np.pi / 7, np.pi / 5, np.pi / 3, np.pi / 2.2],
                         kind="quadratic", assume_sorted=True),
                     "enforceDirectionRatio": (lambda angle: 0)
                  }